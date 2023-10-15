b85f1e7820955c045cf60d3fd624683b8d7168cb
Denis Smet <denis.smetannikov@yandex.ru>
2023-10-15 22:25:06 +0400

    Exercise 1.11

---
+++ src/sicp/chapter_1/ex_1_11.clj
@@ -0,0 +1,28 @@
+(ns sicp.chapter-1.ex-1-11)
+
+; Exercise 1.11
+; A function f is defined by the rule that f(n)=n
+; if n<3 and f(n)=f(n−1)+2f(n−2)+3f(n−3)
+; if n≥3.
+
+; Write a procedure that computes f by means of a recursive process.
+; Write a procedure that computes f by means of an iterative process.
+
+
+(defn f [n]                                                 ; Recursive process
+  (cond (< n 3) n
+        :else (+ (f (- n 1))
+                 (* 2 (f (- n 2)))
+                 (* 3 (f (- n 3))))))
+
+(defn f-v2-iter [a b c count]
+  (if (= count 0)
+    c
+    (f-v2-iter b
+               c
+               (+ c (* 2 b) (* 3 a))
+               (- count 1))))
+
+(defn f-v2 [n]                                              ; Iterative process
+  (cond (< n 3) n
+        :else (f-v2-iter 0 1 2 (- n 2))))
