e016bff231f2457e3d5b80aab6acdd36064882f8
Denis Smetannikov <SmetDenis@users.noreply.github.com>
2023-10-15 23:25:44 +0300

    Exercise 1.19 (#14)

---
+++ test/sicp/chapter_1/ex_1_19_test.clj
@@ -0,0 +1,11 @@
+(ns sicp.chapter-1.ex-1-19-test
+  (:require [clojure.test :refer :all]
+            [sicp.chapter-1.ex-1-19 :refer [fib fib-v2]]))
+
+(deftest fib-test
+  (is (= 1 (fib 1)))
+  (is (= 2 (fib 50))))                                      ; Invalid result! It's expexted
+
+(deftest fib-v2-test
+  (is (= 1 (fib-v2 1)))
+  (is (= 12586269025 (fib-v2 50))))
