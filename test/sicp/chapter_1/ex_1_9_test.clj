4cd0d7845488184f67b6aee7aa568e7e18764e7f
Denis Smet <denis.smetannikov@yandex.ru>
2023-10-15 21:39:05 +0400

    Exercise 1.9

--- test/sicp/chapter_1/ex_1_9_test.clj
+++ test/sicp/chapter_1/ex_1_9_test.clj
@@ -3,9 +3,7 @@
             [sicp.chapter-1.ex-1-9 :refer [plus plus-v2]]))
 
 (deftest plus-test
-  (is (= 3 (plus 1 2)))
-  )
+  (is (= 3 (plus 1 2))))
 
 (deftest plus-v2-test
-  (is (= 3 (plus-v2 1 2)))
-  )
+  (is (= 3 (plus-v2 1 2))))
