332529be7184f4f4b36a0093b9ff74f4b8155fa6
Denis Smet <denis.smetannikov@yandex.ru>
2023-10-21 12:09:14 +0400

    Exercise 1.27

---
+++ test/sicp/chapter_1/part_2/ex_1_27_test.clj
@@ -0,0 +1,11 @@
+(ns sicp.chapter-1.part-2.ex-1-27-test
+  (:require [clojure.test :refer [deftest is]]
+            [sicp.chapter-1.part-2.ex-1-27 :refer [fermat-test]]))
+
+(deftest fermat-test-test
+  (is (= true (fermat-test 561)))
+  (is (= true (fermat-test 1105)))
+  (is (= true (fermat-test 1729)))
+  (is (= true (fermat-test 2465)))
+  (is (= true (fermat-test 2821)))
+  (is (= true (fermat-test 6601))))
