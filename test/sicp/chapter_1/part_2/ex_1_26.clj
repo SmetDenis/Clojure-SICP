4a777ae59de0454f0965b568e259f736571183b7
Denis Smetannikov <SmetDenis@users.noreply.github.com>
2023-10-21 11:51:27 +0400

    Exercise 1.26 - Fixed test (#25)

--- test/sicp/chapter_1/part_2/ex_1_26.clj
+++
@@ -1,3 +0,0 @@
-(ns sicp.chapter-1.part-2.ex-1-26
-  (:require [clojure.test :refer :all]))
-
---
+++ test/sicp/chapter_1/part_2/ex_1_26_test.clj
@@ -0,0 +1,6 @@
+(ns sicp.chapter-1.part-2.ex-1-26-test
+  (:require [clojure.test :refer [deftest is]]
+            [sicp.chapter-1.part-2.ex-1-26 :refer [expmod]]))
+
+(deftest expmod-test
+  (is (= 1 (expmod 2 0 3))))
