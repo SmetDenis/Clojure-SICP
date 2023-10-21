e1435f4e997b0a318af808bae3d5e74c0f51b5d4
Denis Smet <denis.smetannikov@yandex.ru>
2023-10-22 03:20:15 +0400

    Exercise 1.35

--- test/sicp/chapter_1/part_2/ex_1_17_test.clj
+++ test/sicp/chapter_1/part_2/ex_1_17_test.clj
@@ -7,7 +7,7 @@
   (is (= 50 (multi 10 5)))                                  ; "Elapsed time: 1.087389 msecs"
   (is (= 6703582689499996764
          (multi 12345340404 543005091)))                    ; "Elapsed time: 0.852849 msecs"
-  )
+)
 
 (deftest multi-fast-test
   (is (= 2 (multi-fast 1 2)))                               ; "Elapsed time: 1.258788 msecs"
