439ae41282ce33e62ab45b3b09fa721edb2f1579
Denis Smetannikov <SmetDenis@users.noreply.github.com>
2023-10-22 03:22:39 +0400

    Exercise 1.35 (#44)

--- test/sicp/chapter_1/part_2/ex_1_17_test.clj
+++ test/sicp/chapter_1/part_2/ex_1_17_test.clj
@@ -6,12 +6,10 @@
   (is (= 2 (multi 1 2)))                                    ; "Elapsed time: 2.383128 msecs"
   (is (= 50 (multi 10 5)))                                  ; "Elapsed time: 1.087389 msecs"
   (is (= 6703582689499996764
-         (multi 12345340404 543005091)))                    ; "Elapsed time: 0.852849 msecs"
-)
+         (multi 12345340404 543005091))))                   ; "Elapsed time: 0.852849 msecs"
 
 (deftest multi-fast-test
   (is (= 2 (multi-fast 1 2)))                               ; "Elapsed time: 1.258788 msecs"
   (is (= 50 (multi-fast 10 5)))                             ; "Elapsed time: 1.582771 msecs"
   (is (= 6703582689499996764
-         (multi-fast 12345340404 543005091)))               ; "Elapsed time: 1.01612 msecs"
-)
+         (multi-fast 12345340404 543005091))))              ; "Elapsed time: 1.01612 msecs"
---
+++ test/sicp/chapter_1/part_3/ex_1_35_test.clj
@@ -0,0 +1,15 @@
+(ns sicp.chapter-1.part-3.ex-1-35-test
+  (:require [clojure.test :refer [deftest is]]
+            [sicp.chapter-1.part-3.ex-1-35 :refer [gorlden-ration]]))
+
+(deftest gorlden-ration-test
+  (is (= 1.6180327868852458 (gorlden-ration 2)))
+  (is (= 1.6180327868852458 (gorlden-ration 1)))
+  (is (= 1.6180365296803654 (gorlden-ration 0.1)))
+  (is (= 1.6180328133318276 (gorlden-ration 0.01)))
+  (is (= 1.6180327895683542 (gorlden-ration 0.001)))
+  (is (= 1.6180327871539473 (gorlden-ration 0.0001)))
+  (is (= 1.6180327869121198 (gorlden-ration 0.00001)))
+  (is (= 1.6180327868879334 (gorlden-ration 0.000001)))
+  (is (= 1.6180327868855147 (gorlden-ration 0.0000001)))
+  (is (= 1.6180327868852729 (gorlden-ration 0.00000001))))
