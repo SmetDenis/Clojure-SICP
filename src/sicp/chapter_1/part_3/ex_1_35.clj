0ed11e47770b0bf8bf6ccfc65c8b96e497318718
Denis Smet <denis.smetannikov@yandex.ru>
2023-10-22 03:15:48 +0400

    Exercise 1.35

---
+++ src/sicp/chapter_1/part_3/ex_1_35.clj
@@ -0,0 +1,10 @@
+(ns sicp.chapter-1.part-3.ex-1-35
+  (:require [sicp.chapter-1.part-3.book-1-3 :as book]))
+
+; Exercise 1.35
+; Show that the golden ratio φ (1.2.2) is a fixed point of the transformation x↦1+1/x,
+; and use this fact to compute φ by means of the fixed-point procedure.
+
+(defn gorlden-ration
+  [precision]
+  (book/fixed-point #(+ 1.0 (/ 1 %)) precision))
