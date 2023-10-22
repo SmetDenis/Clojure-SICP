0c421addf5e0b444f3d466fc06eb18be72e417e9
Denis Smetannikov <SmetDenis@users.noreply.github.com>
2023-10-22 19:08:44 +0400

    Exercise 1.40 (#50)

---
+++ test/sicp/chapter_1/part_3/ex_1_40_test.clj
@@ -0,0 +1,7 @@
+(ns sicp.chapter-1.part-3.ex-1-40-test
+  (:require [clojure.test :refer [deftest is]]
+            [sicp.chapter-1.part-3.book-1-3 :as book]
+            [sicp.chapter-1.part-3.ex-1-40 :refer [cubic]]))
+
+(deftest cubic-test
+  (is (= -1.2756822036498454 (book/newtons-method (cubic 1 2 3) 1))))
