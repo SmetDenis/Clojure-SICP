1fa43dfd3f2f6c62eb7476034ae337258d80025a
Denis Smetannikov <SmetDenis@users.noreply.github.com>
2023-10-27 02:25:23 +0400

    Exercise 2.16 (#81)

---
+++ src/sicp/chapter_2/part_1/ex_2_16.clj
@@ -0,0 +1,9 @@
+(ns sicp.chapter-2.part-1.ex-2-16)
+
+; Exercise 2.16
+; Explain, in general, why equivalent algebraic expressions may lead to different answers.
+; Can you devise an interval-arithmetic package that does not have this shortcoming,
+; or is this task impossible? (Warning: This problem is very difficult.)
+
+; Every arithmetic operand accumulates error.
+; See http://en.wikipedia.org/wiki/Interval_arithmetic
