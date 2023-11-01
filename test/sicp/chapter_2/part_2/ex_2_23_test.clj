e7cce32a0efca6afc184820a0c01bd8ab908d487
Denis Smet <denis.smetannikov@yandex.ru>
2023-11-02 02:17:02 +0400

    Exercise 2.23

---
+++ test/sicp/chapter_2/part_2/ex_2_23_test.clj
@@ -0,0 +1,7 @@
+(ns sicp.chapter-2.part-2.ex-2-23-test
+  (:require [clojure.test :refer [deftest is]]
+            [sicp.chapter-2.part-2.ex-2-23 :refer [for-each]]))
+
+(deftest for-each-test
+  (is (= true (for-each #(comment (println %))
+                        (list 57 321 88)))))
