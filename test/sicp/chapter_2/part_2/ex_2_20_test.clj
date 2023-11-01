f9039a4690dcf38434fd9ef42a8b67f7716fff01
Denis Smet <denis.smetannikov@yandex.ru>
2023-11-01 22:57:25 +0400

    Exercise 2.20

---
+++ test/sicp/chapter_2/part_2/ex_2_20_test.clj
@@ -0,0 +1,7 @@
+(ns sicp.chapter-2.part-2.ex-2-20-test
+  (:require [clojure.test :refer [deftest is]]
+            [sicp.chapter-2.part-2.ex-2-20 :refer [same-parity]]))
+
+(deftest same-parity-test
+  (is (= '(2 4 6) (same-parity 2 3 4 5 6 7)))
+  (is (= '(1 3 5 7) (same-parity 1 2 3 4 5 6 7))))
