4501d852c0d33dcaa2be6104051c25896d065308
Denis Smet <denis.smetannikov@yandex.ru>
2023-11-02 00:18:43 +0400

    Exercise 2.22

---
+++ test/sicp/chapter_2/part_2/ex_2_22_test.clj
@@ -0,0 +1,6 @@
+(ns sicp.chapter-2.part-2.ex-2-22-test
+  (:require [clojure.test :refer [deftest is]]
+            [sicp.chapter-2.part-2.ex-2-22 :refer [square-list-1 square-list-2]]))
+
+(deftest square-list-1-test
+  (is (= '(16 9 4 1) (square-list-1 (list 1 2 3 4)))))
