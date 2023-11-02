1330646f7df973b07af50e1e24f692e218a56c8a
Denis Smetannikov <SmetDenis@users.noreply.github.com>
2023-11-02 14:35:23 +0400

    Exercise 2.25 (#92)

---
+++ src/sicp/chapter_2/part_2/ex_2_25.clj
@@ -0,0 +1,10 @@
+(ns sicp.chapter-2.part-2.ex-2-25)
+
+; Exercise 2.25
+;
+; Give combinations of cars and cdrs that will pick 7 from each of the following lists:
+; (1 3 (5 7) 9)
+; ((7))
+; (1 (2 (3 (4 (5 (6 7))))))
+
+; See:
