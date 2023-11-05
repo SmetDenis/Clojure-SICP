6fc5294649ccab307c8f99e12c3d837dbf71076a
Denis Smet <denis.smetannikov@yandex.ru>
2023-11-05 22:14:41 +0400

    Exercise 2.35

---
+++ src/sicp/chapter_2/part_2/ex_2_35.clj
@@ -0,0 +1,16 @@
+(ns sicp.chapter-2.part-2.ex-2-35
+  (:require [sicp.chapter-2.part-2.book-2-2 :as b22]
+            [sicp.misc :as m]))
+
+; Exercise 2.35
+; Redefine count-leaves from 2.2.2 as an accumulation:
+;
+; (define (count-leaves t)
+;   (accumulate ⟨??⟩ ⟨??⟩ (map ⟨??⟩ ⟨??⟩)))
+
+(defn count-leaves [tree]
+  (b22/accumulate + 0 (map (fn [sub-tree]
+                             (if (m/pair? sub-tree)
+                               (count-leaves sub-tree)
+                               1))
+                           tree)))
---
+++ test/sicp/chapter_2/part_2/ex_2_35_test.clj
@@ -0,0 +1,9 @@
+(ns sicp.chapter-2.part-2.ex-2-35-test
+  (:require [clojure.test :refer [deftest is]]
+            [sicp.chapter-2.part-2.ex-2-35 :refer [count-leaves]]))
+
+(def tree (cons (list 1 2) (list 3 4)))                     ; ((1 2) 3 4)
+
+(deftest count-leaves-test
+  (is (= 4 (count-leaves tree)))
+  (is (= 2 (count-leaves '(1 2)))))
