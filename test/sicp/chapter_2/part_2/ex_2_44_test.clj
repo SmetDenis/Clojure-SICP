361d9b35c8e148f8d304d40f8065be3e7f81ee23
Denis Smet <denis.smetannikov@yandex.ru>
2023-11-08 03:25:02 +0400

    Book 2.2.4 Example: A Picture Language

---
+++ test/sicp/chapter_2/part_2/ex_2_44_test.clj
@@ -0,0 +1,6 @@
+(ns sicp.chapter-2.part-2.ex-2-44-test
+  (:require [clojure.test :refer [deftest is]])
+  (:require [sicp.chapter-2.part-2.ex-2-44 :refer [up-split]]))
+
+(deftest up-split-test
+  (is (= nil (up-split 1 1)))) ; I can't cover it with unit tests. :(
