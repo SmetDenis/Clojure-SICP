805e7ce45f46e92dff83a6254f67de7e460ff98d
Denis Smet <denis.smetannikov@yandex.ru>
2023-11-09 11:36:47 +0400

    Exercise 2.49

---
+++ src/sicp/chapter_2/part_2/ex_2_49.clj
@@ -0,0 +1,12 @@
+(ns sicp.chapter-2.part-2.ex-2-49)
+
+; Exercise 2.49
+;
+; Use segments->painter to define the following primitive painters:
+;
+; 1. The painter that draws the outline of the designated frame.
+; 2. The painter that draws an “X” by connecting opposite corners of the frame.
+; 3. The painter that draws a diamond shape by connecting the midpoints of the sides of the frame.
+; 4. The wave painter.
+
+
