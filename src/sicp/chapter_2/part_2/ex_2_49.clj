0fdd7def78f360ab521d839cb1cd4651f9fe6a66
Denis Smet <denis.smetannikov@yandex.ru>
2023-11-14 23:20:05 +0400

    Exercise 2.49

--- src/sicp/chapter_2/part_2/ex_2_49.clj
+++ src/sicp/chapter_2/part_2/ex_2_49.clj
@@ -1,4 +1,7 @@
-(ns sicp.chapter-2.part-2.ex-2-49)
+(ns sicp.chapter-2.part-2.ex-2-49
+  (:require [sicp.chapter-2.part-2.book-2-2 :as b22]
+            [sicp.chapter-2.part-2.ex-2-46 :as ex-2-46]
+            [sicp.chapter-2.part-2.ex-2-48 :as ex-2-48]))
 
 ; Exercise 2.49
 ;
@@ -9,4 +12,18 @@
 ; 3. The painter that draws a diamond shape by connecting the midpoints of the sides of the frame.
 ; 4. The wave painter.
 
+(defn outline-painter []
+  (b22/segments->painter [(ex-2-48/make-segment (ex-2-46/make-vect 0 0) (ex-2-46/make-vect 1 0))
+                          (ex-2-48/make-segment (ex-2-46/make-vect 1 0) (ex-2-46/make-vect 1 1))
+                          (ex-2-48/make-segment (ex-2-46/make-vect 1 1) (ex-2-46/make-vect 0 1))
+                          (ex-2-48/make-segment (ex-2-46/make-vect 0 1) (ex-2-46/make-vect 0 0))]))
 
+(defn x-painter []
+  (b22/segments->painter [(ex-2-48/make-segment (ex-2-46/make-vect 0 0) (ex-2-46/make-vect 1 1))
+                          (ex-2-48/make-segment (ex-2-46/make-vect 0 1) (ex-2-46/make-vect 1 0))]))
+
+(defn diamond-painter []
+  (b22/segments->painter [(ex-2-48/make-segment (ex-2-46/make-vect 0.5 0) (ex-2-46/make-vect 1 0.5))
+                          (ex-2-48/make-segment (ex-2-46/make-vect 1 0.5) (ex-2-46/make-vect 0.5 1))
+                          (ex-2-48/make-segment (ex-2-46/make-vect 0.5 1) (ex-2-46/make-vect 0 0.5))
+                          (ex-2-48/make-segment (ex-2-46/make-vect 0 0.5) (ex-2-46/make-vect 0.5 0))]))
---
+++ test/sicp/chapter_2/part_2/ex_2_49_test.clj
@@ -0,0 +1,14 @@
+(ns sicp.chapter-2.part-2.ex-2-49-test
+  (:require [clojure.test :refer [deftest is]]
+            [sicp.chapter-2.part-2.ex-2-49 :refer [outline-painter x-painter diamond-painter]]))
+
+; I don't know to test it properly...
+
+(deftest outline-painter-test
+  (is (= true (fn? (outline-painter)))))
+
+(deftest x-painter-test
+  (is (= true (fn? (x-painter)))))
+
+(deftest diamond-painter-test
+  (is (= true (fn? (diamond-painter)))))
