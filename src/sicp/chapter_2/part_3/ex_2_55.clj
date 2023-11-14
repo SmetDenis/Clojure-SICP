3e734dc551e104bcfdf56c9d13e6bbfdd944d09b
Denis Smet <denis.smetannikov@yandex.ru>
2023-11-15 01:09:04 +0400

    Exercise 2.55

---
+++ src/sicp/chapter_2/part_3/ex_2_55.clj
@@ -0,0 +1,17 @@
+(ns sicp.chapter-2.part-3.ex-2-55
+  (:require [sicp.misc :as m]))
+
+; Exercise 2.55
+;
+; Eva Lu Ator types to the interpreter the expression
+; (car ''abracadabra)
+; To her surprise, the interpreter prints back quote. Explain.
+
+; Apostrophe represents the function `(quote)`
+
+(m/car ''abracadabra)                                       ; quote
+(m/car (quote (quote abasdfa)))                             ; quote
+(m/car '(quote abasdfa))                                    ; quote
+
+(defn print-test []
+  (m/car ''abracadabra))
