559789124284dc0145e1b53d2ed4df09f7a4ea3a
Denis Smet <denis.smetannikov@yandex.ru>
2023-11-14 23:41:02 +0400

    Exercise 2.50

---
+++ test/sicp/chapter_2/part_2/ex_2_50_test.clj
@@ -0,0 +1,14 @@
+(ns sicp.chapter-2.part-2.ex-2-50-test
+  (:require [clojure.test :refer [deftest is]]
+            [sicp.chapter-2.part-2.ex-2-50 :refer [flip-horiz rotate-180-cc rotate-270-cc]]))
+
+; I don't know to test it properly...
+
+(deftest flip-horiz-test
+  (is (= true (fn? (flip-horiz (fn [_] ()))))))
+
+(deftest rotate-180-cc-test
+  (is (= true (fn? (rotate-180-cc (fn [_] ()))))))
+
+(deftest rotate-270-cc-test
+  (is (= true (fn? (rotate-270-cc (fn [_] ()))))))
