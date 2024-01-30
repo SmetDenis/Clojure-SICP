486ea6fde494b5c2236701c05b7bb72af7c325a6
Denis Smet <denis.smetannikov@yandex.ru>
2024-01-30 22:28:06 +0400

    Enhance set operations with tree representation
    This commit enhances set operations by implementing the use of a tree representation. It includes definitions of tree-related functions such as `entry`, `left-branch`, `right-branch` and `make-tree`. Additionally, the functions `element-of-set-tree?` and `adjoin-set-tree` have been introduced to perform set operations. Complementary tests have been added for these new functions.

--- test/sicp/chapter_2/part_3/book_2_3_test.clj
+++ test/sicp/chapter_2/part_3/book_2_3_test.clj
@@ -3,16 +3,21 @@
             [sicp.chapter-2.part-3.book-2-3 :refer [adjoin-set
                                                     deriv
                                                     element-of-set-sorted?
+                                                    element-of-set-tree?
                                                     element-of-set?
+                                                    entry
                                                     intersection-set
                                                     intersection-set-sorted
-                                                    memq]]))
+                                                    left-branch
+                                                    make-tree
+                                                    memq
+                                                    right-branch]]))
 
 (comment "2.3")
-; Symbolic Data ------------------------------------------------------------------------------------
+; symbolic data ------------------------------------------------------------------------------------
 
 (comment "2.3.1")
-; Quotation ----------------------------------------------------------------------------------------
+; quotation ----------------------------------------------------------------------------------------
 
 (deftest memq-test
   (is (= false (memq 'apple '(pear banana prune))))
@@ -20,7 +25,7 @@
   (is (= '(apple pear) (memq 'apple '(x (apple sauce) y apple pear)))))
 
 (comment "2.3.2")
-; Example: Symbolic Differentiation ----------------------------------------------------------------
+; example: symbolic differentiation ----------------------------------------------------------------
 
 (deftest deriv-test
   (is (= 1 (deriv '(+ x 3) 'x)))
@@ -29,7 +34,7 @@
          (deriv '(* (* x y) (+ x 3)) 'x))))
 
 (comment "2.3.3")
-; Example: Representing Sets -----------------------------------------------------------------------
+; example: representing sets -----------------------------------------------------------------------
 
 (deftest element-of-set?-test
   (is (= true (element-of-set? 1 '(1 2 3))))
@@ -60,3 +65,22 @@
   (is (= '(3 4) (intersection-set-sorted '(3 4 5) '(3 4))))
   (is (= '(4) (intersection-set-sorted '(4) '(3 4))))
   (is (= '(15 16 17 18 19) (intersection-set-sorted (range 20) (range 15 25)))))
+
+(deftest make-tree-test
+  (is (= '(1 () ()) (make-tree 1 '() '())))
+  (is (= '(1 (1 2 3) (4 5 6)) (make-tree 1 '(1 2 3) '(4 5 6)))))
+
+(deftest right-branch-test
+  (is (= '(3) (right-branch (make-tree 1 '(2) '(3))))))
+
+(deftest left-branch-test
+  (is (= '(2) (left-branch (make-tree 1 '(2) '(3))))))
+
+(deftest entry-test
+  (is (= 1 (entry (make-tree 1 '(2) '(3))))))
+
+(deftest element-of-set-tree?-test
+  (is (= true (element-of-set-tree? 2 (make-tree 2 '(1) '(3)))))
+  (is (= true (element-of-set-tree? 2 (make-tree 2 '(1) '(3)))))
+  (is (= true (element-of-set-tree? 3 (make-tree 2 '(1) '(3)))))
+  (is (= false (element-of-set-tree? 0 (make-tree 2 '(1) '(3))))))
