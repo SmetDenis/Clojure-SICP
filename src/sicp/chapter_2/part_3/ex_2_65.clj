d092706d74fd72e1ae783b7cff66cee782979b5c
Denis Smet <denis.smetannikov@yandex.ru>
2024-01-31 00:02:26 +0400

    Refactor variable names in Exercise 2.65
    Renamed variables for clarity and conciseness in the implementation of union-set-tree and intersection-set-tree functions. This refactor enhances readability and overall code maintenance in a significant portion of the codebase.

--- src/sicp/chapter_2/part_3/ex_2_65.clj
+++ src/sicp/chapter_2/part_3/ex_2_65.clj
@@ -10,46 +10,50 @@
   (cond
     (empty? tree1) tree2
     (empty? tree2) tree1
-    :else (let [tree1-entry        (b23/entry tree1)
-                tree1-left-branch  (b23/left-branch tree1)
-                tree1-right-branch (b23/right-branch tree1)
-                tree2-entry        (b23/entry tree2)
-                tree2-left-branch  (b23/left-branch tree2)
-                tree2-right-branch (b23/right-branch tree2)]
+    :else (let [tree1-entry (b23/entry tree1)
+                tree1-left  (b23/left-branch tree1)
+                tree1-right (b23/right-branch tree1)
+                tree2-entry (b23/entry tree2)
+                tree2-left  (b23/left-branch tree2)
+                tree2-right (b23/right-branch tree2)]
             (cond
               (= tree1-entry tree2-entry) (b23/make-tree tree1-entry
-                                                         (union-set-tree tree1-left-branch tree2-left-branch)
-                                                         (union-set-tree tree1-right-branch tree2-right-branch))
+                                                         (union-set-tree tree1-left tree2-left)
+                                                         (union-set-tree tree1-right tree2-right))
               (< tree1-entry tree2-entry) (b23/make-tree tree2-entry
-                                                         (union-set-tree tree1 tree2-left-branch)
-                                                         tree2-right-branch)
+                                                         (union-set-tree tree1 tree2-left)
+                                                         tree2-right)
               :else (b23/make-tree tree1-entry
-                                   (union-set-tree tree1-left-branch tree2)
-                                   tree1-right-branch)))))
+                                   (union-set-tree tree1-left tree2)
+                                   tree1-right)))))
 
 (defn intersection-set-tree [tree1 tree2]
   (cond
     (empty? tree1) '()
     (empty? tree2) '()
-    :else (let [tree1-entry        (b23/entry tree1)
-                tree1-left-branch  (b23/left-branch tree1)
-                tree1-right-branch (b23/right-branch tree1)
-                tree2-entry        (b23/entry tree2)
-                tree2-left-branch  (b23/left-branch tree2)
-                tree2-right-branch (b23/right-branch tree2)]
+    :else (let [tree1-entry (b23/entry tree1)
+                tree1-left  (b23/left-branch tree1)
+                tree1-right (b23/right-branch tree1)
+                tree2-entry (b23/entry tree2)
+                tree2-left  (b23/left-branch tree2)
+                tree2-right (b23/right-branch tree2)]
             (cond
-              (= tree1-entry tree2-entry)
-              (b23/make-tree tree1-entry
-                             (intersection-set-tree tree1-left-branch tree2-left-branch)
-                             (intersection-set-tree tree1-right-branch tree2-right-branch))
-              (< tree1-entry tree2-entry)
-              (union-set-tree
-                (intersection-set-tree tree1-right-branch
-                                       (b23/make-tree tree2-entry '() tree2-right-branch))
-                (intersection-set-tree (b23/make-tree tree1-entry tree1-left-branch '())
-                                       tree2-left-branch))
+              (= tree1-entry tree2-entry) (b23/make-tree tree1-entry
+                                                         (intersection-set-tree tree1-left tree2-left)
+                                                         (intersection-set-tree tree1-right tree2-right))
+              (< tree1-entry tree2-entry) (union-set-tree
+                                            (intersection-set-tree tree1-right
+                                                                   (b23/make-tree
+                                                                     tree2-entry
+                                                                     '()
+                                                                     tree2-right))
+                                            (intersection-set-tree (b23/make-tree
+                                                                     tree1-entry
+                                                                     tree1-left
+                                                                     '())
+                                                                   tree2-left))
               :else (union-set-tree
-                      (intersection-set-tree (b23/make-tree tree1-entry '() tree1-right-branch)
-                                             tree2-right-branch)
-                      (intersection-set-tree tree1-left-branch
-                                             (b23/make-tree tree2-entry tree2-left-branch '())))))))
+                      (intersection-set-tree (b23/make-tree tree1-entry '() tree1-right)
+                                             tree2-right)
+                      (intersection-set-tree tree1-left
+                                             (b23/make-tree tree2-entry tree2-left '())))))))
