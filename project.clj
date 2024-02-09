cc311f753025031d6d1a639421ea56d513aaf277
Denis Smet <denis.smetannikov@yandex.ru>
2024-02-09 19:46:34 +0400

    Update GitHub Actions and switch to Leiningen build system
    Modified GitHub Actions to add a new job for running Clojure tests with Leiningen. Also, switched the project build system from Clojure DEPS to Leiningen, necessary files have been added and modified. Updated .gitignore and .editorconfig files to align with these changes.

---
+++ project.clj
@@ -0,0 +1,15 @@
+(defproject smetdenis/clojure-sicp "0.1"
+  :description "Solving exercises from SICP with Clojure"
+  :url "https://github.com/SmetDenis/Clojure-SICP"
+  :license {:name "MIT"
+            :url  "http://opensource.org/licenses/MIT"}
+  :dependencies [[org.clojure/clojure "1.11.1"]]
+  :source-paths ["src"]
+  :test-paths ["test"]
+  :main ^:skip-aot sicp
+  :target-path "build/%s"
+  :profiles {:dev      {:dependencies [[jonase/eastwood "1.4.2"]]}
+             :test     {:main-opts ["-m" "cognitect.test-runner"]}
+             :eastwood {:main-opts ["-m" "eastwood.lint" {}]}
+             :uberjar  {:aot :all}
+             :jvm-opts ["-Dclojure.compiler.direct-linking=true"]})
