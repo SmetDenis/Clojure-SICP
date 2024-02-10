1d97e67108e37ed9451cd7b6418b7259985ad92a
Denis Smet <denis.smetannikov@yandex.ru>
2024-02-10 13:59:32 +0400

    Revert "Add `lein-kibit` plugin to `project.clj`"
    This reverts commit 2c9694cf4f7eafc322acd42478cd3c4c6c74cc06.

--- project.clj
+++ project.clj
@@ -8,8 +8,7 @@
   :test-paths ["test"]
   :main ^:skip-aot sicp
   :target-path "build/%s"
-  :plugins [[jonase/eastwood "1.4.2"]
-            [lein-kibit "0.1.8"]]
+  :plugins [[jonase/eastwood "1.4.2"]]
   :profiles {:dev      {:dependencies [[jonase/eastwood "1.4.2"]]}
              :test     {:main-opts ["-m" "cognitect.test-runner"]}
              :eastwood {:main-opts ["-m" "eastwood.lint" {}]}
