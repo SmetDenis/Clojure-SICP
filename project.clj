8e322e2da279a6df22d80bab9be5ecbceae55508
Denis Smet <denis.smetannikov@yandex.ru>
2024-02-10 21:41:24 +0400

    Add splint to linting process and update tasks
    Splint has been integrated into the linting process. Changes were made in the Github workflows, project.clj and bb.edn files. Tasks for style checking, linting, and fixing have been reshuffled and updated accordingly.

--- project.clj
+++ project.clj
@@ -9,8 +9,10 @@
   :main ^:skip-aot sicp
   :target-path "build/%s"
   :plugins [[jonase/eastwood "1.4.2"]]
-  :profiles {:dev      {:dependencies [[jonase/eastwood "1.4.2"]]}
+  :profiles {:dev      {:dependencies [[org.clojure/clojure "1.11.1"]
+                                       [io.github.noahtheduke/splint "1.12"]]}
              :test     {:main-opts ["-m" "cognitect.test-runner"]}
              :eastwood {:main-opts ["-m" "eastwood.lint" {}]}
              :uberjar  {:aot :all}
-             :jvm-opts ["-Dclojure.compiler.direct-linking=true"]})
+             :jvm-opts ["-Dclojure.compiler.direct-linking=true"]}
+  :aliases {"splint" ["run" "-m" "noahtheduke.splint"]})
