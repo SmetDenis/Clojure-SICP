8d732c6f9fcfb462d96645edf812d5c9fd9741a6
Denis Smet <denis.smetannikov@yandex.ru>
2024-02-10 21:31:13 +0400

    Update dependencies for Clojure and linting tools
    The lein-kibit plugin has been removed and replaced with splint linter. Clojure has also been updated to version 1.11.1. Corresponding changes have been made to the testing and linting tasks in bb.edn to accommodate the updated configurations.

--- project.clj
+++ project.clj
@@ -8,10 +8,12 @@
   :test-paths ["test"]
   :main ^:skip-aot sicp
   :target-path "build/%s"
-  :plugins [[jonase/eastwood "1.4.2"]
-            [lein-kibit "0.1.8"]]
-  :profiles {:dev      {:dependencies [[jonase/eastwood "1.4.2"]]}
+  :plugins [[jonase/eastwood "1.4.2"]]
+  :profiles {:dev      {:dependencies [[io.github.noahtheduke/splint "1.12"]
+                                       [jonase/eastwood "1.4.2"]
+                                       [org.clojure/clojure "1.11.1"]]}
              :test     {:main-opts ["-m" "cognitect.test-runner"]}
              :eastwood {:main-opts ["-m" "eastwood.lint" {}]}
              :uberjar  {:aot :all}
-             :jvm-opts ["-Dclojure.compiler.direct-linking=true"]})
+             :jvm-opts ["-Dclojure.compiler.direct-linking=true"]}
+  :aliases {"splint" ["run" "-m" "noahtheduke.splint"]})
