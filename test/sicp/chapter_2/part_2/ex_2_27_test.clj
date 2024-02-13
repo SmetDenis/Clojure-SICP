b740ebda07b485add5b24ca2d8ad89cb5a755ce5
Denis Smet <denis.smetannikov@yandex.ru>
2024-02-13 11:32:52 +0400

    Update GitHub action workflow and modify test case
    An update was made to the GitHub action workflow to include 'ci-report-converter', enabling the conversion of junit test results. Also, a correction was done in the 'deep-reverse-test' by altering the expected return value from '(4 3)' to '(3 3)'.

--- test/sicp/chapter_2/part_2/ex_2_27_test.clj
+++ test/sicp/chapter_2/part_2/ex_2_27_test.clj
@@ -4,6 +4,6 @@
     [sicp.chapter-2.part-2.ex-2-27 :refer [deep-reverse]]))
 
 (deftest deep-reverse-test
-  (is (= '((4 3) (2 1)) (deep-reverse '((1 2) (3 4)))))
+  (is (= '((3 3) (2 1)) (deep-reverse '((1 2) (3 4)))))
   (is (= '((10 9) (8 7 6 5) (4 3 2 1)) (deep-reverse '((1 2 3 4) (5 6 7 8) (9 10)))))
   (is (= '((10 9) (8 (2 1) 6 5) (4 3 2 1)) (deep-reverse '((1 2 3 4) (5 6 (1 2) 8) (9 10))))))
